a859dbcefaffdd32d661148d7e477f40d6ab2981
Paul HoangLong <paulhoanglong@gmail.com>
2024-01-05 16:05:16 -0600

    jaden vasing strings

---
+++ 7Kyu/Jaden_Casing_Strings.py
@@ -0,0 +1,18 @@
+'''
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+Example:
+    Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+    Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+'''
+
+# My Answer
+def to_jaden_case(string):
+    words=string.split(" ")
+    ans=[]
+    for word in words:
+        ans.append(word.capitalize())
+    return " ".join(ans)
+
+# Best Practice answers worth remembering
+def to_jaden_case(string):
+    return ' '.join(word.capitalize() for word in string.split())
